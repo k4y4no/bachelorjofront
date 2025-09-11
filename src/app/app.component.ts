c545cb2005b44a13be8004e957fa8e68716a40b8
k4y4 <k4y4no@gmail.com>
2025-09-11 12:26:52 +0200

    navbar responsive v1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'jofront';
+  isMenuOpen:boolean = false;
+
+  toggleMenu(): void{
+    this.isMenuOpen = !this.isMenuOpen
+  }
 }
