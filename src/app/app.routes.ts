d04438470632af3bdb2f6f2cf8814f47d18f0334
k4y4no <85989182+k4y4no@users.noreply.github.com>
2025-09-16 15:10:35 +0200

    Router init + basic component (home/offers/contact)
    * Router started
    
    * add home/offers/contact components to test router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { OffersComponent } from './pages/offers/offers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'offers', component: OffersComponent },
+];
