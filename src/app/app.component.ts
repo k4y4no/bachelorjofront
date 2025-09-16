5a05f76d3b80440e814768f7f97fadbfca655189
k4y4no <85989182+k4y4no@users.noreply.github.com>
2025-09-16 15:21:29 +0200

    Feature/navbar (#5)
    * navbar responsive v1.0
    
    * navbar responsive v1.5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'jofront';
+  isMenuOpen:boolean = false;
+  innerWidth: number = 0;
+
+  ngOnInit(): void {
+    this.innerWidth = window.innerWidth
+    console.log(this.innerWidth)
+  }
+
+  toggleMenu(): void{
+    if(this.innerWidth > 400){
+      return
+    }
+    this.isMenuOpen = !this.isMenuOpen
+    console.log(window.innerWidth)
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event:Event) {
+    this.innerWidth = window.innerWidth;
+        if(this.innerWidth > 400){
+          this.isMenuOpen = false
+    }
+  }
 }
