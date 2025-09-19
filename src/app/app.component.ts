30a414f75addf717f706c003c46cc1863b5151a4
k4y4 <k4y4no@gmail.com>
2025-09-19 18:21:10 +0200

    Update navigation to auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, HostListener, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, HostListener, inject, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterModule, Event } from '@angular/router';
+import { AuthService } from './_services/auth/auth.service';
+import { TokenService } from './_services/token/token.service';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +11,35 @@ import { RouterModule } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
+  router = inject(Router)
+  authService = inject(AuthService)
+  tokenService = inject(TokenService)
   title = 'jofront';
   isMenuOpen:boolean = false;
   innerWidth: number = 0;
+  isAuthenticated: boolean | null = null;
+  isAdmin: boolean | null = null;
+  logMenu: string = "login"
+  profileOn: string = "register"
+
+  constructor(){
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this.toggleMenu()
+        this.isAuthenticated = this.authService.isAuthenticated();
+        this.isAdmin = this.authService.isAdmin(this.tokenService.getRoleToken(this.authService.getToken()))
+        this.logMenu = this.isAuthenticated ? "logout":"login"
+        if(this.isAuthenticated) {
+          this.profileOn = this.isAdmin ? "dash":"profile"
+        } else {
+          this.profileOn = "register"
+        }
+        console.log(this.isAuthenticated)
+        console.log(this.isAdmin)
+
+      }
+    })
+  }
 
   ngOnInit(): void {
     this.innerWidth = window.innerWidth
@@ -23,7 +51,12 @@ export class AppComponent implements OnInit{
       return
     }
     this.isMenuOpen = !this.isMenuOpen
-    console.log(window.innerWidth)
+
+  }
+
+  loggingOut(){
+    this.authService.logout();
+    this.router.navigate(['login'])
   }
 
   @HostListener('window:resize', ['$event'])
