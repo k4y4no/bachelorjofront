0c1cfa8261a98003217f8430574099d93f04e89f
k4y4no <85989182+k4y4no@users.noreply.github.com>
2025-09-21 10:01:36 +0200

    Display offers v1.00 - Basic offer datalist (#9)

---
+++ src/app/_interfaces/offer.ts
@@ -0,0 +1,14 @@
+export interface Offer {
+    id: number;
+    name: string
+
+}
+
+export interface DataSingleOffer {
+    data: Offer
+
+}
+
+export interface DataOffers {
+    data: Offer[]
+}
--- src/app/pages/offers/offers.component.ts
+++ src/app/pages/offers/offers.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { OfferService } from '../../_services/offer/offer.service';
+import { Observable } from 'rxjs';
+import { Offer } from '../../_interfaces/offer';
 
 @Component({
   selector: 'app-offers',
-  imports: [],
+  imports: [
+    AsyncPipe
+  ],
   templateUrl: './offers.component.html',
   styleUrl: './offers.component.css'
 })
-export class OffersComponent {
+export class OffersComponent implements OnInit{
+  private OfferService: OfferService = inject(OfferService)
 
+  offers$!: Observable<Offer[]>;
+
+  constructor () {
+
+  }
+
+
+  ngOnInit(): void {
+        this.offers$ = this.OfferService.getAllOffers()
+    }
 }
